70fdfbb665262ee4a685eebebffc824f6c463457
benjamin-mlsi <benjamin.mulamisini@web.de>
2025-02-27 21:40:27 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Entfernen Sie diese Zeile, falls sie vorhanden ist, um dynamische Routen zu ermöglichen
+  output: "export",
+};
+
+module.exports = nextConfig;
